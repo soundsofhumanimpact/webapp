26052456e98763f9ddd17391b07edf881702e26e
milesccoleman <milesccoleman@gmail.com>
2020-06-08 18:42:03 -0400

    buttons and phasing continued

--- src/components/SoundsOfImpact.vue
+++ src/components/SoundsOfImpact.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
+    <h2>{{ msg2 }}</h2>
     <ul id="birds" >
-      <li><img class="card" :alt="birdName1" :src="birdImage1">{{birdName1}}</li>
-      <li><img class="card" :alt="birdName2" :src="birdImage2">{{birdName2}}</li>
-      <li><img class="card" :alt="birdName3" :src="birdImage3">{{birdName3}}</li>
-      <li><img class="card" :alt="birdName4" :src="birdImage4">{{birdName4}}</li>
+      <li v-show="card1"><img class="card" :alt="birdName1" :src="birdImage1">{{birdName1}}</li>
+      <li v-show="card2"><img class="card" :alt="birdName2" :src="birdImage2">{{birdName2}}</li>
+      <li v-show="card3"><img class="card" :alt="birdName3" :src="birdImage3">{{birdName3}}</li>
+      <li v-show="card4"><img class="card" :alt="birdName4" :src="birdImage4">{{birdName4}}</li>
     </ul>
     <button v-if="!isHidden" v-on:click="isHidden=true; generateSoundscape()">Generate Soundscape</button>
-    <button v-if="isHidden">1970</button>
-    <button v-if="isHidden">2020</button>
+    <button v-if="isHidden" v-on:click="nineteenSeventy">1970</button>
+    <button v-if="isHidden" v-on:click="twentyTwenty">2020</button>
     <button v-if="isHidden" v-on:click="isHidden=false; reset">reset</button>
   </div>
 </template>
@@ -21,25 +22,33 @@ export default {
   name: 'SoundsOfImpact',
   data () {
     return {
-      msg: '', 
+      msg: '',
+      msg2: '',  
       birdName1: '', 
       birdImage1: '', 
       birdSound1: '',
       birdSound1Pan: '',
+      birdAudio1: '',
       birdName2: '', 
       birdImage2: '', 
       birdSound2: '',
-      birdSound2Pan: '', 
+      birdSound2Pan: '',
+      birdAudio2: '', 
       birdName3: '', 
       birdImage3: '', 
       birdSound3: '',
-      birdSound3Pan: '', 
+      birdSound3Pan: '',
+      birdAudio3: '', 
       birdName4: '', 
       birdImage4: '', 
       birdSound4: '',
       birdSound4Pan: '',
-      context: true,
-      isHidden: false   
+      birdAudio4: '',
+      isHidden: false, 
+      card1: false, 
+      card2: false, 
+      card3: false, 
+      card4: false   
     }
   },
 created: function () {
@@ -48,52 +57,74 @@ created: function () {
     reset: function () {
      },
      generateSoundscape: function () {
-     const self = this; 
+     var self = this; 
      Pizzicato.context.resume();
        axios.get("https://raw.githubusercontent.com/soundsofhumanimpact/data/master/birdData.json")
        .then(function (response) { 
        
           self.birdName1 = response.data.raptors[1][0]
           self.birdImage1 = response.data.raptors[1][2]
-          self.birdSound1Pan = new Pizzicato.Effects.StereoPanner({pan: Math.random()*2 - 1});
           self.birdSound1 = new Pizzicato.Sound(response.data.raptors[1][Math.floor(Math.random()*4+3)], function() {
+          self.birdSound1Pan = new Pizzicato.Effects.StereoPanner({pan: Math.random()*2 - 1});
           self.birdSound1.addEffect(self.birdSound1Pan);
           self.birdSound1.volume = Math.random(); 
-          self.birdSound1.play();});
+          self.birdAudio1 = self.birdSound1.clone();
+          });
           
           self.birdName2 =  response.data.raptors[3][0]
           self.birdImage2 = response.data.raptors[3][2]
-          self.birdSound2Pan = new Pizzicato.Effects.StereoPanner({pan: Math.random()*2 - 1});
           self.birdSound2 = new Pizzicato.Sound(response.data.raptors[3][Math.floor(Math.random()*4+3)], function() {
+          self.birdSound2Pan = new Pizzicato.Effects.StereoPanner({pan: Math.random()*2 - 1});
           self.birdSound2.volume = Math.random(); 
           self.birdSound2.addEffect(self.birdSound2Pan);
-          self.birdSound2.play();});
+          self.birdAudio2 = self.birdSound2.clone();
+          });
           
           self.birdName3 =  response.data.finches[1][0]
           self.birdImage3 = response.data.finches[1][2]
-          self.birdSound3Pan = new Pizzicato.Effects.StereoPanner({pan: Math.random()*2 - 1});
           self.birdSound3 = new Pizzicato.Sound(response.data.finches[1][Math.floor(Math.random()*4+3)], function() {
+          self.birdSound3Pan = new Pizzicato.Effects.StereoPanner({pan: Math.random()*2 - 1});
           self.birdSound3.volume = Math.random(); 
           self.birdSound3.addEffect(self.birdSound3Pan);
-          self.birdSound3.play();});
+          self.birdAudio3 = self.birdSound3.clone();
+          });
           
           self.birdName4 =  response.data.finches[3][0]
           self.birdImage4 = response.data.finches[3][2]
-          self.birdSound4Pan = new Pizzicato.Effects.StereoPanner({pan: Math.random()*2 - 1});
           self.birdSound4 = new Pizzicato.Sound(response.data.finches[3][Math.floor(Math.random()*4+3)], function() {
+          self.birdSound4Pan = new Pizzicato.Effects.StereoPanner({pan: Math.random()*2 - 1});
           self.birdSound4.volume = Math.random(); 
           self.birdSound4.addEffect(self.birdSound4Pan);
-          self.birdSound4.play();});
-          self.primeScape()
+          self.birdAudio4 = self.birdSound4.clone();
+          });
+        self.msg = "Soundscape Variables Generated"
+        self.msg2 = "Select a Time Period to Listen"
        })
        .catch(function (error) {
        console.log(error);
        })
     }, 
-    primeScape: function (){
-      if (this.context == true){
-        alert(this.birdName1)
-      } 
+    nineteenSeventy: function (){
+       this.card2 = false; 
+       this.card3 = false;
+       this.birdAudio2.pause();
+       this.birdAudio3.pause();
+       
+       this.card1 = true; 
+       this.card4 = true;
+       this.birdAudio1.play();
+       this.birdAudio4.play(); 
+    },
+    twentyTwenty: function (){
+       this.card1 = false; 
+       this.card4 = false; 
+       this.birdAudio1.pause();
+       this.birdAudio4.pause();
+       
+       this.card2 = true; 
+       this.card3 = true;
+       this.birdAudio2.play();
+       this.birdAudio3.play(); 
     }
   }
 }
